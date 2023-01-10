b2552b8cef182c3379a6f0136597d29317bf7b7a
Michael-Liangz <Michaelliangz.ml@gmail.com>
2023-01-10 19:49:35 +0700

    updated

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// TODO =============== SHOW MENU ===============
+
+// TODO =============== REMOVE MENU MOBILE ===============
+
+// TODO =============== CHANGE BACKGROUND HEADER ===============
+
+// TODO =============== POPULAR SWIPER ===============
+
+// TODO =============== MIXITUP FILTER FEATURED ===============
+
+// * Link active featured
+
+// TODO =============== SHOW SCROLL UP ===============
+
+// TODO =============== SCROLL SECTIONS ACTIVE LINK ===============
+
+// TODO =============== SCROLL REVEAL ANIMATION ===============
