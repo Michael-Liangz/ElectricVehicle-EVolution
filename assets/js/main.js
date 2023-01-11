3f05a04101684abb47675b95c674eb72b1cfae6c
Michael-Liangz <Michaelliangz.ml@gmail.com>
2023-01-11 17:01:44 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,35 @@
 // TODO =============== SHOW MENU ===============
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+// * =============== MENU SHOW ===============
+// * Validate if constant exits
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// * =============== MENU HIDDEN ===============
+// * Validate if constant exits
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 // TODO =============== REMOVE MENU MOBILE ===============
+const navLink = document.querySelectorAll(".nav_link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // * When we click on each nav_link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((listlinkMenu) =>
+  listlinkMenu.addEventListener("click", linkAction)
+);
 
 // TODO =============== CHANGE BACKGROUND HEADER ===============
 
