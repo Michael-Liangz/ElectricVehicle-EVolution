0fddba1eab89757a674ad220fbb170f4ad89c225
Michael-Liangz <Michaelliangz.ml@gmail.com>
2023-01-15 20:43:37 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,7 +87,39 @@ linkFeatured.forEach((carCategory) =>
 );
 
 // TODO =============== SHOW SCROLL UP ===============
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // * When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-thumb
+  if (this.scrollY >= 350) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUp.classList.remove("show-scroll");
+  }
+}
+window.addEventListener("scroll", scrollUp);
 
 // TODO =============== SCROLL SECTIONS ACTIVE LINK ===============
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
 
 // TODO =============== SCROLL REVEAL ANIMATION ===============
